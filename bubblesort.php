5ac44502dad4b05b7b51f48c20519b1ded338d4c
mahusaygeorgemarquez <110375.mahusay@gmail.com>
2017-04-28 22:12:11 +0800

    init bubble sort

---
+++ bubblesort.php
@@ -0,0 +1,21 @@
+<?php 
+
+function bubbleSort($list = array()){
+	for($i=0; $i<count($list)-1; $i++){
+		for($j=$i; $j<count($list)-1; $j++){
+			if($list[$i] > $list[$j]){
+				$tmp = $list[$i];
+				$list[$i] = $list[$j];
+				$list[$j] = $tmp;
+			}
+		}
+	}
+	
+	return $list;
+}
+
+$list = array(1,3,5,22,4,80,100,135,150);
+$list = bubbleSort($list,0,0);
+
+var_dump($list);
+?>
