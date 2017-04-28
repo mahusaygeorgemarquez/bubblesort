ae7da31db66803a43cac10a6641e4f27d553ba8d
mahusaygeorgemarquez <110375.mahusay@gmail.com>
2017-04-28 22:13:22 +0800

    Update bubblesort.php

--- bubblesort.php
+++ bubblesort.php
@@ -2,7 +2,7 @@
 
 function bubbleSort($list = array()){
 	for($i=0; $i<count($list)-1; $i++){
-		for($j=$i; $j<count($list)-1; $j++){
+		for($j=$i; $j<count($list); $j++){
 			if($list[$i] > $list[$j]){
 				$tmp = $list[$i];
 				$list[$i] = $list[$j];
